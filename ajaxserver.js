9bd3649a3cbb15821497501541f7db312228eb95
Dave Pape <depape@buffalo.edu>
2023-05-11 06:56:58 -0700

    more ajax

--- ajaxserver.js
+++ ajaxserver.js
@@ -1,14 +1,33 @@
 "use strict";
 
+let savedInfo = { dummy: 1 };
+
 function increment(req, res)
     {
     let answer = { 'value': parseInt(req.params.num)+1 };
     res.json(answer);
     }
 
+function saveinfo(req, res)
+    {
+    console.log('saveinfo');
+    console.log(req.body);
+    savedInfo[req.body.name] = req.body.value;
+    let answer = { 'value': parseInt(req.params.num)+1 };
+    res.send('saved');
+    }
+
+function getinfo(req, res)
+    {
+    console.log('got request for info');
+    res.json(savedInfo);
+    }
+
 const express = require('express');
 let router = express.Router();
 
 router.get('/increment/:num', increment);
+router.post('/saveinfo', saveinfo);
+router.get('/getinfo', getinfo);
 
 module.exports = router;
